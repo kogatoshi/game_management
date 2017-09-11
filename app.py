86fbd1380fdc6bd1d35fe545fec3a625a579bb89
Tenma Endou <hyper.tenmakun@docomo.ne.jp>
2017-09-12 00:44:24 +0900

    9/11

--- app.py
+++ app.py
@@ -1,65 +1,104 @@
 from flask import Flask, render_template, abort, request
-from flask import session, redirect, url_for
-from flask_httpauth import HTTPBasicAuth
+from flask import redirect, url_for
+from flask_httpauth import HTTPDigestAuth
 from game.models import Hardware, Games
+from game.database import db_session
 import os
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-auth = HTTPBasicAuth()
+auth = HTTPDigestAuth()
 
-users = {"john": "hello", "susan": "bye"}
+session = db_session()
 
-
-@app.before_request
-def before_request():
-    if session.get('username') is not None:
-        return
-    if request.path == '/login':
-        return
-    return redirect('/login')
+users = {"john": "hello", "susan": "bye", "tenmaendou": "tenma129"}
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST' and _is_account_valid():
-        session['username'] = request.form['username']
-        return redirect(url_for('game'))
-    return render_template('login.html')
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
 
 
-def _is_account_valid():
-    username = request.form.get('username')
-    if username == 'admin':
-        return True
-    return False
+# 最初のページ
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('login'))
-
-
-@app.route("/")
+# ログイン
+@app.route('/login')
 @auth.login_required
+def login():
+    if auth.username() == "tenmaendou":
+        return redirect(url_for('manage'))
+    else:
+        return render_template("login.html", username=auth.username())
+
+
+# ソフト・ハードの一覧
+@app.route("/game")
 def game():
     hard_contents = Hardware.query.all()
     soft_contents = Games.query.all()
     return render_template(
-            "index.html",
-            hard_contents=hard_contents,
-            soft_contents=soft_contents
-        )
+                "game_list.html",
+                hard_contents=hard_contents,
+                soft_contents=soft_contents,
+                username=auth.username()
+            )
 
 
+# 詳細ページ
 @app.route("/<name>", methods=["GET"])
 def show_content(name):
     content = Hardware.query.filter_by(name=name).first()
     if content is None:
         abort(404)
     return render_template("show_content.html", content=content)
+
+
+# 管理ページ
+@app.route("/manage")
+def manage():
+    hard_contents = Hardware.query.all()
+    soft_contents = Games.query.all()
+    hard_contents = Hardware.query.all()
+    return render_template(
+                "manage.html",
+                hard_contents=hard_contents,
+                soft_contents=soft_contents,
+            )
+
+
+# ソフトをデータベースに追加
+@app.route("/addsoft", methods=["POST"])
+def addsoft():
+    # テキストボックスからソフトの名前を取得
+    title = request.form["softName"]
+    # チェックボックスからハードウェアのリストを取得
+    hardnumbers = request.form.getlist("hardNumbers")
+    game = Games(title=title)
+    hards = []
+    for hard in hardnumbers:
+        name = session.query(Hardware).filter_by(id=hard).one().name
+        hards.append(Hardware(name=name))
+    game.hardwares.extend(hards)
+
+    session.add(game)
+    session.commit()
+    return "ok"  # redirect(url_for('manage'))
+    """
+    bf4 = Games(title='Battlefield3')
+    ps3 = session.query(Hardware).filter_by(id=3).one()
+    ps4 = session.query(Hardware).filter_by(id=4).one()
+    bf4.hardwares.extend([ps3, ps4])
+    session.add(bf4)
+    session.commit()
+    return "ok"
+    """
 
 
 if __name__ == "__main__":
--- game/models.py
+++ game/models.py
@@ -1,12 +1,28 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from game.database import Base
+
+
+games_hardware_table = Table(
+                    'games_hardwares',
+                    Base.metadata,
+                    Column('game_id', Integer, ForeignKey('games.id')),
+                    Column('hard_id', Integer, ForeignKey('hardware.id')),
+                )
 
 
 class Games(Base):
     __tablename__ = 'games'
     id = Column(Integer, primary_key=True)
     title = Column(String(128), unique=True)
-    hardware = Column(Text)
+
+    hardwares = relationship(
+            'Hardware',
+            order_by='Hardware.id',
+            uselist=True,
+            backref='games',
+            secondary=games_hardware_table
+    )
 
     def __init__(self, title=None, hardware=None):
         self.title = title
@@ -26,3 +42,16 @@ class Hardware(Base):
 
     def __repr__(self):
         return '<Name %r>' % (self.name)
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(16), unique=True)
+    password = Column(String(32))
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
